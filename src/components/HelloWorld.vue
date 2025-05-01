5ad8c8eda852f553e95523b32cb6e887259b3097
Johan03Guerrero <sebas03h@gmail.com>
2025-04-30 21:37:20 -0500

    feat: reinstall shadcn-vue with button and input components

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-defineProps<{ msg: string }>()
-
 const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1 class="font-bold text-red-500 text-3xl underline">
+    Hello world!
+  </h1>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
